a435c2f6269a2a9a3aa528d158b4e9b90286c41b
pacellyjose@gmail.com <pacellyjose@gmail.com@a0251069-ea2e-bdea-3e2c-5ccb4205a2e2>
2013-05-30 09:00:03 +0000

    Backup Antes Da Implementação JSF

--- trunk/PortalDaTransparencia/src/br/com/millercs/servlets/ContextListenerServlet.java
+++ trunk/PortalDaTransparencia/src/br/com/millercs/servlets/ContextListenerServlet.java
@@ -14,10 +14,12 @@ public class ContextListenerServlet implements ServletContextListener {
 	
 	
 	private ConnectionPool connectionPool = null;
-	
+
+	@SuppressWarnings("unused")
 	//Esse método é chamado uma única vez, ao servidor de aplicação startar
 	public void contextInitialized(ServletContextEvent event) {
 		this.connectionPool = ConnectionPool.getConnectionPool();
+		
 		Config config = new Config();
 		Language language = new PT_BR();
 		UrlRobot urlRobot = new UrlRobot();
